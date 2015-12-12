2463ffb3547f31c2f6f72531d3f3ca7d2198075f
Nick Germaine <nickgermaine1024@gmail.com>
2015-12-12 11:35:34 -0400

    adding header

--- vinyl-music/main.h
+++ vinyl-music/main.h
@@ -1,22 +1,5 @@
 #ifndef MAIN
 #define MAIN
-class DataObject : public QObject
-{
-    Q_OBJECT
 
-    Q_PROPERTY(QString name READ name WRITE setName)
-    Q_PROPERTY(QString color READ color WRITE setColor)
-
-    QString name;
-    QString color;
-
-    void setName(QString name) { this->name = name;}
-    void setColor(QString color) {this->color = color;}
-    // getters
-
-    QString getName() {return name;}
-    QString getColor() {return color;}
-
-};
 #endif // MAIN
 
