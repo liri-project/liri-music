a320635b903f8af17c702260d05f1d5c56f366f8
Nick Germaine <nickgermaine1024@gmail.com>
2015-12-14 13:33:46 -0400

    song object update

--- vinyl-music/main.h
+++ vinyl-music/main.h
@@ -1,40 +1,40 @@
-    #ifndef SONG_OBJECT_MAIN
-    #define SONG_OBJECT_MAIN
+#ifndef SONG_OBJECT_MAIN
+#define SONG_OBJECT_MAIN
 
-    #include <QObject>
+#include <QObject>
 
-    class SongObject : public QObject
-    {
-        Q_OBJECT
+class SongObject : public QObject
+{
+    Q_OBJECT
 
-        Q_PROPERTY(QString path READ path CONSTANT)
-        Q_PROPERTY(QString title READ title CONSTANT)
-        Q_PROPERTY(QString album READ album CONSTANT)
-        Q_PROPERTY(QString artist READ artist CONSTANT)
-        Q_PROPERTY(QString art READ art CONSTANT)
-        QString m_path;
-        QString m_title;
-        QString m_album;
-        QString m_artist;
-        QString m_art;
+    Q_PROPERTY(QString path READ path CONSTANT)
+    Q_PROPERTY(QString title READ title CONSTANT)
+    Q_PROPERTY(QString album READ album CONSTANT)
+    Q_PROPERTY(QString artist READ artist CONSTANT)
+    Q_PROPERTY(QString art READ art CONSTANT)
+    QString m_path;
+    QString m_title;
+    QString m_album;
+    QString m_artist;
+    QString m_art;
 
 
-        QString path() { return m_path;}
-        QString title() { return m_title;}
-        QString album() { return m_album;}
-        QString artist() {return m_artist;}
-        QString art() {return m_art;}
+    QString path() { return m_path;}
+    QString title() { return m_title;}
+    QString album() { return m_album;}
+    QString artist() {return m_artist;}
+    QString art() {return m_art;}
 
-    public:
-        explicit SongObject(const QString &path, const QString &title, const QString &album, const QString &artist, const QString &art) :
-            QObject(),
-            m_path(path),
-            m_title(title),
-            m_album(album),
-            m_artist(artist),
-            m_art(art)
-        {}
+public:
+    explicit SongObject(const QString &path, const QString &title, const QString &album, const QString &artist, const QString &art) :
+        QObject(),
+        m_path(path),
+        m_title(title),
+        m_album(album),
+        m_artist(artist),
+        m_art(art)
+    {}
 
-    };
+};
 
-    #endif // SONG_OBJECT_MAIN
+#endif // SONG_OBJECT_MAIN
